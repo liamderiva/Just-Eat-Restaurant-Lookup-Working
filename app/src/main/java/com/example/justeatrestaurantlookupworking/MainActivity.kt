a33f95e6ec64ebda84530626ed36c7defbfba781
liamderiva <lvicari@hotmail.com>
2025-04-24 16:26:10 +0100

    initial commit

---
+++ app/src/main/java/com/example/justeatrestaurantlookupworking/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.justeatrestaurantlookupworking
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.justeatrestaurantlookupworking.ui.theme.JustEatRestaurantLookupWorkingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JustEatRestaurantLookupWorkingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JustEatRestaurantLookupWorkingTheme {
+        Greeting("Android")
+    }
+}
